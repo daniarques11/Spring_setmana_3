11f837268d60b687d3e429041c1e6626ea282df0
unknown <darques@cifpfbmoll.eu>
2020-05-13 22:50:12 +0200

    feat: Afegir CRUD sobre la BDD

--- src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
+++ src/main/java/org/formacio/setmana1/data/LlibreOpsBasic.java
@@ -1,26 +1,45 @@
 package org.formacio.setmana1.data;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
 import org.formacio.setmana1.domini.Llibre;
 import org.formacio.setmana1.domini.Recomanacio;
+import org.springframework.stereotype.Component;
 
 /**
  * Modifica aquesta classe per tal que sigui un component Spring que realitza les 
  * operacions de persistencia tal com indiquen les firmes dels metodes
  */
+
+@Component
 public class LlibreOpsBasic {
 	
+	@PersistenceContext
+	private EntityManager em;
 	/**
 	 * Retorna el llibre amb l'ISBN indicat o, si no existeix, llança un LlibreNoExisteixException
 	 */
+	@Transactional
 	public Llibre carrega (String isbn) throws LlibreNoExisteixException {
-		return null;
+		return em.find(Llibre.class, isbn);
 	}
 	
 	/**
 	 * Sense sorpreses: dona d'alta un nou llibre amb les propietats especificaques
 	 */
+	@Transactional
 	public void alta (String isbn, String autor, Integer pagines, Recomanacio recomanacio, String titol) {
+		Llibre llibre = new Llibre();
+		llibre.setIsbn(isbn);
+		llibre.setAutor(autor);
+		llibre.setPagines(pagines);
+		llibre.setRecomanacio(recomanacio);
+		llibre.setTitol(titol);
+		em.persist(llibre);
+		
 	}
 	
 	/**
@@ -28,14 +47,20 @@ public class LlibreOpsBasic {
 	 * @param isbn del llibre a eliminar
 	 * @return true si s'ha esborrat el llibre, false si no existia
 	 */
+	 @Transactional
+	 // TO-DO afegir possibilitat que no es trobi l'objecte a la BDD
 	public boolean elimina (String isbn) {
+		Llibre eliminar = em.find(Llibre.class, isbn);
+		em.remove(eliminar);
 		return true;
 	}
 	
 	/**
 	 * Guarda a bbdd l'estat del llibre indicat
 	 */
+	 @Transactional
 	public void modifica (Llibre llibre) {
+		 llibre = em.merge(llibre);
 	}
 	
 	/**
